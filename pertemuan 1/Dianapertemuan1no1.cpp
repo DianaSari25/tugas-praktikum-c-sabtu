d60518d1988bfb8319eac7fabc6e0540272e4b80
unknown <dianasari7398@gmail.com>
2023-10-14 15:18:39 +0700

    first commit

---
+++ pertemuan 1/Dianapertemuan1no1.cpp
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+using namespace std;
+
+int main()
+{
+ 	int nim = 233250;
+ 	char nama[] = "Diana Sari";
+ 	char jurusan[] = "Teknik informatika";
+ 	
+	printf ("\n nim saya: %i", nim);
+	printf ("\n nama saya: %s ", nama);
+	printf ("\n jurusan saya: %s ", jurusan);
+	
+	 return 0;
+ }
+	
---
+++ pertemuan 1/Dianapertemuan1no2.cpp
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+int main () {
+
+	int panjang = 10; // panjang dalam cm
+	int lebar = 7;   // lebar dalam cm
+	int luas = panjang * lebar;
+	
+	printf("luas persegi panjang: %cm^2\n", luas);
+	return 0;
+}
